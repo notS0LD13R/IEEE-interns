bb93c199e3b51bee6f2065c4e1ee6d88049a8204
notS0LD13R <joelsebin@gmail.com>
2022-12-24 15:07:40 +0530

    Added navbar

---
+++ script.js
@@ -0,0 +1,27 @@
+// async function post_form(){
+
+//     form={
+//         name:'Tom Holland',
+//         email:'tomholland@gmail.com',
+//         phone:'1234567890',
+//         description:'This is the real Tom',
+//         services:'[]',
+//         formId:'task'
+//     }
+//     console.log(form)
+
+//     const response=await fetch('https://mint-forms.ieee-mint.org/api/form/addresponse',{
+//         method:'POST',
+//         body:new URLSearchParams(form),
+        
+//     })
+//     console.log(response)
+//     const data=await response.json()
+//     console.log(data)
+//     return data
+// }
+
+
+
+
+
