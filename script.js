3082ff85a6e5d45fc0db6b5d0c460253e3e7ba12
notS0LD13R <joelsebin@gmail.com>
2022-12-25 16:59:51 +0530

    Added input value verification

--- script.js
+++ script.js
@@ -36,15 +36,75 @@ burger.addEventListener('click',(e)=>{
 const form=document.getElementById('mainform')
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
-    const form_data=new FormData(form)
-    form_data.append('formId','task')
-    console.log(Array.from(new URLSearchParams(form_data)))
-
+    let form_data=new FormData(form)
+    const check_value=checkdata(form_data)
     
-    post_form(new URLSearchParams(form_data)).then((stat)=>{
-        console.log(stat)
-    })
+
+    if (check_value[0]){
+        show_form_error(check_value[1],reset=true)
+        form_data=convert_data(form_data)
+        console.log(Array.from(form_data))
+        post_form(form_data).then((stat)=>{
+            console.log(stat)
+        })
+    }
+    else
+        show_form_error(check_value[1])
+
+        
 })
+
+function show_form_error(form_error,reset=false){
+    console.log('reset',reset)
+    doms={
+        'name':document.getElementById('Name'),
+        'email':document.getElementById('Email'),
+        'phone':document.getElementById('PhoneNo'),
+        'description':document.getElementById('Description'),
+    }
+    console.log(form_error)
+    console.log(doms)
+    for (i of Object.keys(form_error)){
+        if(reset){
+            doms[i].classList.remove('error');
+        }
+        else if(!form_error[i]){
+            doms[i].classList.toggle('error');
+        }
+    }
+    
+}
+
+function convert_data(form_data){
+    const mainkey=['name','email','phone','description']
+    const services=[]
+    let temp=Array.from(form_data.keys()) 
+    for (i of temp){
+        if (!mainkey.includes(i)){
+            form_data.delete(i)
+            services.push(i)
+        }
+    }
+
+    form_data.append('services',`${services}`)
+    form_data.append('formId','task')
+
+    return new URLSearchParams(form_data) 
+
+}
+
+function checkdata(form_data){
+    const flag={'name':true,'email':true,'phone':true,'description':true}
+    const pattern={'name':/^[a-zA-z ]{2,30}$/g,'email':/\w+@[a-z]+.[a-z]{1,3}$/g,'phone':/^\d{10}$/g,'description':/\w+/g}
+
+    for (i of form_data.keys()){
+        if (i in flag)
+            flag[i]=pattern[i].test(form_data.get(i))
+    }
+    
+    return [Object.values(flag).every(i=>i===true),flag]
+    
+}
 
 
 async function post_form(form_data){
